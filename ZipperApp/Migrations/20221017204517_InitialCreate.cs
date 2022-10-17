d9037720e2b58edc110befaba17bbe8b0dba6cbf
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2022-10-17 17:08:04 -0400

    Add files via upload
    Signed-off-by: Justin Brierley <91489058+paradingwolves@users.noreply.github.com>

---
+++ ZipperApp/Migrations/20221017204517_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZipperApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Zipper",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    Material = table.Column<string>(nullable: true),
+                    TeethSize = table.Column<string>(nullable: true),
+                    Colour = table.Column<string>(nullable: true),
+                    ProductImage = table.Column<string>(nullable: true),
+                    Rating = table.Column<decimal>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zipper", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Zipper");
+        }
+    }
+}
